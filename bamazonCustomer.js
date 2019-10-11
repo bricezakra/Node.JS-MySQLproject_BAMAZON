a039594c28bbeab1266c19583c6f873ff2a73feb
bricezakra <bricezakra@live.com>
2019-10-10 23:54:21 -0500

    js files modified

--- bamazonCustomer.js
+++
@@ -1,2 +0,0 @@
-# Node.JS-MySQLproject_BAMAZON
-BAMAZON is a Node.js command line interface that utilizez MySQL database to create a virtual store experience. Users can take on the role of either customer, manager, or supervisor and interact with the store through different commands.
--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,2 +1,3 @@
-# Node.JS-MySQLproject_BAMAZON
-BAMAZON is a Node.js command line interface that utilizez MySQL database to create a virtual store experience. Users can take on the role of either customer, manager, or supervisor and interact with the store through different commands.
+#
+Node.JS - MySQLproject_BAMAZON
+BAMAZON is a Node.js command line interface that utilizez MySQL database to create a virtual store experience.Users can take on the role of either customer, manager, or supervisor and interact with the store through different commands.
